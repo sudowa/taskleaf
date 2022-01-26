078d1999130d654863310734cf7776ad702f7287
sudowa <s_s_wataru@yahoo.co.jp>
2022-01-26 22:15:58 +0900

    create first

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ ruby "2.7.0"
 gem "slim-rails"
 gem "html2slim"
 gem "bootstrap"
+gem "rails-i18n"
 
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.0.1"
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+
+  root to: 'tasks#index'
+  resources :tasks
+
+  # get 'tasks/index'
+  # get 'tasks/show'
+  # get 'tasks/new'
+  # get 'tasks/edit'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
