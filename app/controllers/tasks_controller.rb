600a08351380bf586347b9560782bdb18b5c1c69
sudowa <s_s_wataru@yahoo.co.jp>
2022-01-28 17:39:29 +0900

    削除処理完了（link_to→button_to)

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -30,7 +30,7 @@ class TasksController < ApplicationController
   def destroy
     task = Task.find(params[:id])
     task.destroy
-    redirect_to task_url, notice: "タスク「#{task.name}」を削除しました。"
+    redirect_to tasks_url, notice: "タスク「#{task.name}」を削除しました。"
     
   end
 
